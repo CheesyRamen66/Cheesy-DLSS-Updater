e77c637b6979193e1046025e04cf3877d11da846
Matthieu Seay <matthieu.seay@gmail.com>
2025-01-31 16:03:29 -0500

    collects dlss tweaks and dll in temp folder

--- dlss_updater.py
+++ dlss_updater.py
@@ -1,33 +1,67 @@
 import os
-import requests
-from bs4 import BeautifulSoup
-from packaging import version
+import shutil
 import urllib.request
 import tempfile
 import glob
+import json
+import requests
+import zipfile
 
-DLSS_VERSIONS_URL = "https://www.techpowerup.com/download/nvidia-dlss-dll/"
-GITHUB_DLL_URL="https://github.com/NVIDIA/DLSS/blob/main/lib/Windows_x86_64/rel/nvngx_dlss.dll"
+DLSS_TAGS_URL = "https://api.github.com/repos/NVIDIA/DLSS/tags"
+DLSS_DLL_URL="https://raw.githubusercontent.com/NVIDIA/DLSS/main/lib/Windows_x86_64/rel/nvngx_dlss.dll"
 DEFAULT_DLL_NAME="nvngx_dlss.dll"
+NEXUS_MOD_DOMAIN="site"
+NEXUS_MOD_ID=550
+NEXUS_FILE_URL=f'https://api.nexusmods.com/v1/games/{NEXUS_MOD_DOMAIN}/mods/{NEXUS_MOD_ID}/files'
 
-def get_dlss_version():
-    def fetch_dlss_page():
-        # Fetches the HTML content of the DLSS download page.
-        response = requests.get(DLSS_VERSIONS_URL)
-        response.raise_for_status()
-        return response.text
+def fetch_latest_dlss_tag():
+    # Fetches the latest DLSS tag from the NVIDIA DLSS GitHub repository.
+    response = requests.get(DLSS_TAGS_URL)
+    if response.status_code == 200:
+        tags = response.json()
+        if tags:
+            latest_version = tags[0]["name"]
+            print(f"Latest NVIDIA DLSS Tag: {latest_version}")
+        else:
+            print("No tags found.")
+    else:
+        print(f"Failed to fetch tags: {response.status_code}, {response.text}")
 
-    html_content = fetch_dlss_page()
+temp_dir = tempfile.TemporaryDirectory()
+print('Created temporary directory', temp_dir)
 
-    soup = BeautifulSoup(html_content, 'html.parser')
-    for header in soup.find_all('h3'):
-        if 'NVIDIA DLSS DLL' in header.text:
-            sibling_div = header.find_next_sibling('div')
-            if sibling_div and sibling_div.find('span', class_='flag latest'):
-                return header.text.strip()
+if os.path.exists(DEFAULT_DLL_NAME):
+    shutil.copy(DEFAULT_DLL_NAME, temp_dir.name)
+else:
+    fetch_latest_dlss_tag()
+    urllib.request.urlretrieve(DLSS_DLL_URL, temp_dir.name+"/"+DEFAULT_DLL_NAME)
 
-print("We're updating to "+get_dlss_version())
-with tempfile.TemporaryDirectory() as tempdirname:
-    print('Created temporary directory', tempdirname)
-    urllib.request.urlretrieve(GITHUB_DLL_URL, tempdirname+"/"+DEFAULT_DLL_NAME)
-    print(glob.glob(tempdirname+"/*"))
+tweaks_folders = glob.glob("DLSSTweaks*/")
+if tweaks_folders:
+    tweaks_folders.sort()
+    shutil.copytree(tweaks_folders[-1], temp_dir.name+"/DLSSTweaks")
+
+else:
+    tweaks_zips = glob.glob("DLSSTweaks*.zip")
+    if tweaks_zips:
+        tweaks_zips.sort()
+        with zipfile.ZipFile(tweaks_zips[-1], 'r') as zip_ref:
+            zip_ref.extractall(temp_dir.name+"/DLSSTweaks")
+
+    else:
+        with open('credentials.json', 'r') as credentials_file:
+            NEXUS_API_KEY = json.load(credentials_file)['nexus_api_key']
+            nexus_headers = {
+                'apikey': NEXUS_API_KEY
+            }
+            response = requests.get(NEXUS_FILE_URL+f'.json', headers=nexus_headers)
+            response.raise_for_status()
+            file_id = response.json()['files'][-1]['id'][0]
+            response = requests.get(NEXUS_FILE_URL+f'/{file_id}/download_link.json', headers=nexus_headers)
+            response.raise_for_status()
+            tweaks_download_link = response.json()[0]['URI']
+            urllib.request.urlretrieve(tweaks_download_link, temp_dir.name+"/DLSSTweaks.zip")
+            with zipfile.ZipFile(temp_dir.name+"/DLSSTweaks.zip", 'r') as zip_ref:
+                zip_ref.extractall(temp_dir.name+"/DLSSTweaks")
+
+print(os.listdir(temp_dir.name))
